cbb04159df6af6a03112499d768f3d32ed10b0df
marchboy <jjgdut@163.com>
2018-05-14 00:21:13 +0800

    ''

--- python_test/test.py
+++ python_test/test.py
@@ -55,15 +55,31 @@ print(len(names))
 # http://bbs.fishc.com/thread-79821-1-1.html
 # https://blog.csdn.net/ly_ysys629/article/details/55224284
 import pandas as pd
+import random
 
 df = pd.DataFrame(
     {"total_bill":[16.99, 10.34, 23.68, 23.68, 24.59],
     "tip":[1.01, 1.66, 3.50, 3.31, 3.61],
     "sex":['Female', 'Male', 'Male', 'Male', 'Female']})
 
-print(df.dtypes, '\n')
-print(df.index, '\n')
-print(df.columns, '\n')
-print(df.values, '\n')
-print(df)
+# print(df.dtypes, '\n')
+# print(df.index, '\n')
+# print(df.columns, '\n')
+# print(df.values, '\n')
+# print(df)
+
+print('-' * 50)
+rnd_1 = [random.randrange(1,20) for x in range(1000)]
+rnd_2 = [random.randrange(1,20) for x in range(1000)]
+rnd_3 = [random.randrange(1,20) for x in range(1000)]
+fecha = pd.date_range('2012-4-10', '2015-1-4')
+
+data = pd.DataFrame({'fecha':fecha, 'rnd_1':rnd_1, 'rnd_2':rnd_2, 'rnd_3':rnd_3})
+print(data)
+print(data.describe())
+
+print(data[0:10][["fecha", "rnd_1"]])
+print(data[0:10])
+print('-' * 50)
+print(data.loc[1:5])  # 推荐此方法进行行列选择，而不是[]
 
