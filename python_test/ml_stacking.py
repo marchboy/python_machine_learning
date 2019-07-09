a7bcfc066a6d463ac65e3c41170f9b270180b527
jinjun.gui <jjgdut@163.com>
2019-07-09 10:59:49 +0800

    updated

--- python_test/ml_stacking.py
+++ python_test/ml_stacking.py
@@ -3,6 +3,8 @@
 # date:2019-04-30
 # Model Stacking
 # https://www.cnblogs.com/jiaxin359/p/8559029.html
+# http://wulc.me/2018/01/21/stacking%20%E7%9A%84%E5%9F%BA%E6%9C%AC%E6%80%9D%E6%83%B3%E5%8F%8A%E4%BB%A3%E7%A0%81%E5%AE%9E%E7%8E%B0/
+
 
 import numpy as np
 from sklearn.model_selection import KFold
